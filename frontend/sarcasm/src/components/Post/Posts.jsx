69bcdd9b9e8e0d972edcfda819ff8cc8ec6e6c40
Md Mahabubul Alam Abir <mahbubabir09@gmail.com>
2024-06-03 11:03:24 +0600

    Modified

--- frontend/sarcasm/src/components/Post/Posts.jsx
+++ frontend/sarcasm/src/components/Post/Posts.jsx
@@ -25,13 +25,12 @@ const Posts = ({ post }) => {
       <div className="card-container">
         <div className="card card-custom">
           
-          <div className="card-front card-body  position-relative">
-          
+          <div className="card-front card-body position-relative">
+            <p className="sentiment-text">{sentiment}</p>
             <img src={anonymonus} alt="Card Image 1" />
           </div>
           <div className="card-back card-body">
             <p className="card-text">{post.post_content}</p>
-            <p>{sentiment}</p>
           </div>
         </div>
       </div>
