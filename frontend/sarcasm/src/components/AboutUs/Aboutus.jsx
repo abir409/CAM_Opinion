fc8ee92dfd89207de910db53329f029ad9df483f
Md Mahabubul Alam Abir <mahbubabir09@gmail.com>
2024-06-10 20:46:04 +0600

    image added

--- frontend/sarcasm/src/components/AboutUs/Aboutus.jsx
+++ frontend/sarcasm/src/components/AboutUs/Aboutus.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Aboutus.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import anonym from '../../images/anonym.png';
+import KarimBhai from '../../images/KarimBhai.jpeg';
+import JalisBhai from '../../images/JalisBhai.jpeg';
+import ZubaerBhai from '../../images/ZubaerBhai.jpeg';
+import ShakilBhai from '../../images/ShakilBhai.jpeg';
+import abir from '../../images/abir.jpeg';
 
 const Aboutus = () => {
   return (
@@ -9,7 +13,7 @@ const Aboutus = () => {
       <h1>About Us</h1>
       <div className="row">
         <div className="col-md-12">
-          <img src={anonym} alt="Team Leader" className="img-fluid leader-img"/>
+          <img src={KarimBhai} alt="Team Leader" className="img-fluid leader-img"/>
           <div className="person-info">
             <h3>John Doe</h3>
             <p>Team Leader<br/>Engineering Department</p>
@@ -19,28 +23,28 @@ const Aboutus = () => {
       <div className="developer-images">
         <div className="row">
           <div className="col-md-3">
-            <img src={anonym} alt="Developer 1" className="img-fluid developer-img"/>
+            <img src={JalisBhai} alt="Developer 1" className="img-fluid developer-img"/>
             <div className="person-info">
               <h3>Jane Smith</h3>
               <p>Software Developer<br/>Engineering Department</p>
             </div>
           </div>
           <div className="col-md-3">
-            <img src={anonym} alt="Developer 2" className="img-fluid developer-img"/>
+            <img src={ZubaerBhai} alt="Developer 2" className="img-fluid developer-img"/>
             <div className="person-info">
               <h3>Michael Johnson</h3>
               <p>Frontend Developer<br/>Design Department</p>
             </div>
           </div>
           <div className="col-md-3">
-            <img src={anonym} alt="Developer 3" className="img-fluid developer-img"/>
+            <img src={ShakilBhai} alt="Developer 3" className="img-fluid developer-img"/>
             <div className="person-info">
               <h3>Sarah Williams</h3>
               <p>Backend Developer<br/>Engineering Department</p>
             </div>
           </div>
           <div className="col-md-3">
-            <img src={anonym} alt="Developer 4" className="img-fluid developer-img"/>
+            <img src={abir} alt="Developer 4" className="img-fluid developer-img"/>
             <div className="person-info">
               <h3>David Brown</h3>
               <p>Full Stack Developer<br/>Engineering Department</p>
