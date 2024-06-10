619e49930e43f29767c795ea6506e0f59e1e4b76
Md Mahabubul Alam Abir <mahbubabir09@gmail.com>
2024-06-10 15:41:06 +0600

    About Us updated

--- frontend/sarcasm/src/components/AboutUs/Aboutus.jsx
+++ frontend/sarcasm/src/components/AboutUs/Aboutus.jsx
@@ -1,62 +1,56 @@
 import React from 'react';
 import './Aboutus.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import anonym from '../../images/anonym.png';
 
 const Aboutus = () => {
   return (
-    <div>
-      <div className="container">
-        {/* Head of Department Section */}
-        <div className="profile" style={{ color: 'azure' }}>
-          <img src="images/anonym.png" alt="Head of Department" width="400"/>
-          <h3>Md. Momtajul Karim</h3>
-          <h4>Head of Capital Market</h4>
-          <div className="speech-bubble">
-            <p>We Are Anonymous. We Are Legion. We Do Not Forgive. We Do Not Forget. Expect Us.</p>
+  <div className="aboutus">
+      <h1>About Us</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <img src={anonym} alt="Team Leader" className="img-fluid leader-img"/>
+          <div className="person-info">
+            <h3>John Doe</h3>
+            <p>Team Leader<br/>Engineering Department</p>
           </div>
         </div>
-        
-        {/* Team Members Section */}
-        <div className="row team" style={{ textAlign: 'center' }}>
+      </div>
+      <div className="developer-images">
+        <div className="row">
           <div className="col-md-3">
-            <div className="card">
-              <img src="images/anonym.png" className="card-img-top" alt="Member 1" width="100"/>
-              <div className="card-body">
-                <h8 className="card-title">Jalis Mahmud Tarif</h8>
-                <p className="card-text">Software Engineer</p>
-              </div>
+            <img src={anonym} alt="Developer 1" className="img-fluid developer-img"/>
+            <div className="person-info">
+              <h3>Jane Smith</h3>
+              <p>Software Developer<br/>Engineering Department</p>
             </div>
           </div>
           <div className="col-md-3">
-            <div className="card">
-              <img src="images/anonym.png" className="card-img-top" alt="Member 2" width="100"/>
-              <div className="card-body">
-                <h8 className="card-title">Md. Zubaer Ahmed</h8>
-                <p className="card-text">Jr. Software Engineer</p>
-              </div>
+            <img src={anonym} alt="Developer 2" className="img-fluid developer-img"/>
+            <div className="person-info">
+              <h3>Michael Johnson</h3>
+              <p>Frontend Developer<br/>Design Department</p>
             </div>
           </div>
           <div className="col-md-3">
-            <div className="card">
-              <img src="images/anonym.png" className="card-img-top" alt="Member 3" width="100"/>
-              <div className="card-body">
-                <h8 className="card-title">Md. Mahbubul Alam Abir</h8>
-                <p className="card-text">Jr. Software Engineer</p>
-              </div>
+            <img src={anonym} alt="Developer 3" className="img-fluid developer-img"/>
+            <div className="person-info">
+              <h3>Sarah Williams</h3>
+              <p>Backend Developer<br/>Engineering Department</p>
             </div>
           </div>
           <div className="col-md-3">
-            <div className="card">
-              <img src="images/anonym.png" className="card-img-top" alt="Member 4" width="100"/>
-              <div className="card-body">
-                <h8 className="card-title">Abdus Shakil Chowdhury</h8>
-                <p className="card-text">Software Engineer</p>
-              </div>
+            <img src={anonym} alt="Developer 4" className="img-fluid developer-img"/>
+            <div className="person-info">
+              <h3>David Brown</h3>
+              <p>Full Stack Developer<br/>Engineering Department</p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+ 
+  );
 }
 
 export default Aboutus;
